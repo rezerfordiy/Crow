c79a4e2b504e6d432a6d834ebf8bc707c4659a2c
Artem <aartemskij@bk.ru>
2025-09-06 22:35:02 +0300

    fix(mistake): idk how it happened

--- SceneManager.cpp
+++
@@ -1,6 +0,0 @@
-#include "SceneManager.h"
-#include "MySceneManager.h"
-
-std::unique_ptr<SceneManager> SceneManager::create(MyScene* scene) {
-    return std::make_unique<MySceneManager>(scene);
-}
